819e71c70643aee618a349bfc32d354a51469d08
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-09-16 19:37:13 -0500

    feat(app): update routing structure with Layout component and add Plans and Summary pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './styles/main.scss'
 import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout.tsx'
 import HomePage from './pages/HomePage.tsx'
+import PlansPage from './pages/PlansPage.tsx'
+import SummaryPage from './pages/SummaryPage.tsx'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      {/*<Route path="/plans" element={<PlansPage />} />*/}
-      {/*<Route path="/summary" element={<SummaryPage />} />*/}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/plans" element={<PlansPage />} />
+        <Route path="/summary" element={<SummaryPage />} />
+      </Route>
     </Routes>
   )
 }
